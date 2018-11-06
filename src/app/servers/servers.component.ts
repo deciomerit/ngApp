d61b5c59731126f4c803422fbc4c52746ca0d916
decioMerit <dsales@meritmile.com>
2018-11-06 15:23:30 -0500

    first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+
+export class ServersComponent implements OnInit {
+
+	allowNewServer = false;
+	serverCreationStatus = 'No Server was created';
+	serverName = 'onelove';
+
+  constructor() {
+  	setTimeout(() => {
+  		this.allowNewServer = true;
+  	}, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+  	this.serverCreationStatus = 'Server was Created';
+  }
+
+  onUpdateServerName(event: Event){
+  	this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
